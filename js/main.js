80279857d719a5eadbba1822902c4918d94520c6
truong2305 <truongnv18dt1bkdn@gmail.com>
2021-05-02 11:16:54 +0700

    add

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.client__slider').slick({
+        dots: true,
+        arrows:false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+    $('.header__btn-menu').click(function(){
+        $('.header__btn-menu').toggleClass("close-menu");
+        $('.main__nav').toggleClass("main__nav--open")
+      });
+    $('.main__nav--contact-click').click(function(){
+        $(".main__nav--contact").slideToggle("500");
+      });
+})
